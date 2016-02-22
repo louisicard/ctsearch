dee84951c7c6b4db93860a39cc39b7c820a874c4
Louis Sicard <lsicard@core-techs.fr>
2016-02-22 17:45:15 +0100

    Bug related to non XML utf8 characters in OAI harvester

--- src/CtSearchBundle/Datasource/OAIHarvester.php
+++ src/CtSearchBundle/Datasource/OAIHarvester.php
@@ -129,7 +129,7 @@ class OAIHarvester extends Datasource {
     if(isset($response['headers']['set-cookie'])){
       $this->cookies = $response['headers']['set-cookie'];
     }
-    return $response['content'];
+    return preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $response['content']);
   }
 
   private function parseHttpResponse($string) {
