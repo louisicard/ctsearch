c55dec31177c053785bf07caf11c58c1186909e6
Louis Sicard <lsicard@core-techs.fr>
2016-03-17 12:07:56 +0100

    Updating HmeController so a proper message is displayed when Elastic server is unreachable

--- src/CtSearchBundle/Controller/HomeController.php
+++ src/CtSearchBundle/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace CtSearchBundle\Controller;
 
+use Elasticsearch\Common\Exceptions\NoNodesAvailableException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,16 +14,24 @@ class HomeController extends Controller {
    * @Route("/", name="homepage")
    */
   public function indexAction(Request $request) {
-    
-    $info = IndexManager::getInstance()->getElasticInfo();
-    
-    $serverInfo = IndexManager::getInstance()->getServerInfo();
+    try {
+
+      $info = IndexManager::getInstance()->getElasticInfo();
+
+      $serverInfo = IndexManager::getInstance()->getServerInfo();
+    }
+    catch(NoNodesAvailableException $ex){
+      $info = null;
+      $serverInfo = null;
+      $noMenu = true;
+    }
 
     return $this->render('ctsearch/homepage.html.twig', array(
           'title' => $this->get('translator')->trans('Welcome to Ct search'),
           'info' => $info,
           'server_info' => $serverInfo,
           'main_menu_item' => 'home',
+          'no_menu' => isset($noMenu) && $noMenu ? true : false,
     ));
   }
 
