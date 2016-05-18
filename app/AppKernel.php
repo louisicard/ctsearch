86c0d9814a741dd78202f87a0137e428c96bd776
louisicard <lsicard@core-techs.fr>
2016-05-18 11:15:02 +0200

    Cleaning sources for migrating to Symfony 3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -7,19 +7,18 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new CtSearchBundle\CtSearchBundle(),
-        );
+        $bundles = [
+          new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+          new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+          new Symfony\Bundle\TwigBundle\TwigBundle(),
+          new Symfony\Bundle\MonologBundle\MonologBundle(),
+          new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+          new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+          new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+          new CtSearchBundle\CtSearchBundle(),
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -27,6 +26,21 @@ class AppKernel extends Kernel
         }
 
         return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
